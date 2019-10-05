c946938fae8451314fc11e2d654b7534f0ccf5d1
YusukeKyokawa <kiyo@dot1x6071.14-192-19.wlan.ms.dendai.ac.jp>
2019-10-05 17:55:46 +0900

    detect_squares.jpg

--- Project/detect_squares.py
+++ Project/detect_squares.py
@@ -1,10 +1,5 @@
 from __future__ import print_function
 import sys
-PY3 = sys.version_info[0] == 3
-
-if PY3:
-    xrange = range
-
 import numpy as np
 import cv2 
 
@@ -14,22 +9,26 @@ def angle_cos(p0, p1, p2):
     return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
 
 def find_squares(img):
+    # ノイズ除去
     img = cv2.GaussianBlur(img, (5, 5), 0)
     squares = []
     for gray in cv2.split(img):
-        for thrs in xrange(0, 255, 26):
+        for thrs in range(0, 255, 26
+        ):
             if thrs == 0:
                 bin = cv2.Canny(gray, 0, 50, apertureSize=5)
                 bin = cv2.dilate(bin, None)
             else:
+                # 画像二値化
                 _retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
+            # 矩形領域を抽出
             contours, _hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
             for cnt in contours:
                 cnt_len = cv2.arcLength(cnt, True)
                 cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
-                if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+                if len(cnt) == 4 and cv2.contourArea(cnt) > 500 and cv2.isContourConvex(cnt):
                     cnt = cnt.reshape(-1, 2)
-                    max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
+                    max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in range(4)])
                     if max_cos < 0.1:
                         squares.append(cnt)
     return squares
@@ -39,11 +38,10 @@ def main(img_path):
     for fn in glob(img_path):
         img = cv2.imread(fn)
         squares = find_squares(img)
-        contours_img = cv2.drawContours( img, squares, -1, (0, 255, 0), 3 )
+        contours_img = cv2.drawContours(img, squares, -1, (0, 255, 0), 3 )
         cv2.imwrite("./squares.jpg", contours_img)
         # cv2.imshow('squares', img)
         # ch = cv2.waitKey()
-
     print('Done')
 
 
