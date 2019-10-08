8cbe41718353bf6f4870984c23578f1d5559a4e0
YusukeKyokawa <kiyo@YusukeKyokawanoMacBook-Pro.local>
2019-10-08 09:25:14 +0900

    ふぃｃ

--- Project/detect_squares.py
+++ Project/detect_squares.py
@@ -4,6 +4,18 @@ import numpy as np
 import cv2 
 
 
+# for (i,cnt) in enumerate(contours_large):
+#     arclen = cv2.arcLength(cnt, True)
+#     approx = cv2.approxPolyDP(cnt, 0.02*arclen, True)
+#     if len(approx) < 4:
+#         continue
+#     approxes.append(approx)
+#     rect = getRectByPoints(approx)
+#     rects.append(rect)
+#     outputs.append(getPartImageByRect(rect))
+#     cv2.imwrite('./out/output'+str(i)+'.jpg', getPartImageByRect(rect))
+
+
 def angle_cos(p0, p1, p2):
     d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
     return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
@@ -44,9 +56,28 @@ def main(img_path):
         # ch = cv2.waitKey()
     print('Done')
 
+
+def getRectByPoints(points):
+    # prepare simple array 
+    points = list(map(lambda x: x[0], points))
+
+    points = sorted(points, key=lambda x:x[1])
+    top_points = sorted(points[:2], key=lambda x:x[0])
+    bottom_points = sorted(points[2:4], key=lambda x:x[0])
+    points = top_points + bottom_points
+
+    left = min(points[0][0], points[2][0])
+    right = max(points[1][0], points[3][0])
+    top = min(points[0][1], points[1][1])
+    bottom = max(points[2][1], points[3][1])
+    return (top, bottom, left, right)
+
+
+
 def detect_squares(img):
     # img = cv2.imread(img_path)
     squares = find_squares(img)
+    crop_contours(squares)
     contours_img = cv2.drawContours(img, squares, -1, (0, 255, 0), 3)
     return contours_img
 
